c84ff539dfdefd90c2c36466bb575bc52b1c7edb
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-01-31 21:27:19 +0100

    Finishing the Navbar, Starting the Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Stack } from "@mui/material";
-import Logo from "../assets/images/Logo.png";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import Exercises from "../components/Exercises";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
 
 const Home = () => {
     return (
-        <Stack
-            direction="row"
-            justifyContent="space-around"
-            sx={{
-                gap: { sm: "122px", xs: "40px" },
-                mt: { sm: "32px", xs: "20px" },
-            }}
-        >
-            <Link to="/">
-                <img
-                    src={Logo}
-                    alt="logo"
-                    style={{ width: "48px", height: "48px", margin: "0 20px" }}
-                />
-            </Link>
-            <Stack
-                direction="row"
-                gap="40px"
-                fontSize="24px"
-                alignItems="flex-end"
-            >
-                <Link
-                    to="/"
-                    style={{
-                        textDecoration: "none",
-                        color: "#3A1212",
-                        borderBottom: "3px solid #FF2625",
-                    }}
-                >
-                    Home
-                </Link>
-                <a
-                    href="#exercises"
-                    style={{ textDecoration: "none", color: "#3A1212" }}
-                >
-                    Exercises
-                </a>
-            </Stack>
-        </Stack>
+        <Box>
+            <HeroBanner />
+            <SearchExercises />
+            <Exercises />
+        </Box>
     );
 };
 
