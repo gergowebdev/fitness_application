6c1f91c0829893e6f2f5ea04f717ff045807f945
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-02-03 20:10:18 +0100

    Fetching data from rapidAPI

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const SearchExercises = () => {
+    const [search, setSearch] = useState("");
+    const handleSearch = async () => {
+        if (search) {
+            const exercisesData = await fetchData(
+                "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+                exerciseOptions
+            );
+
+            console.log(exercisesData);
+        }
+    };
+
     return (
         <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
             <Typography
@@ -23,8 +36,10 @@ const SearchExercises = () => {
                         width: { lg: "800px", xs: "350px" },
                     }}
                     height="76px"
-                    value=""
-                    onChange={(e) => {}}
+                    value={search}
+                    onChange={(e) =>
+                        setSearch(e.target.value.toLocaleLowerCase())
+                    }
                     placeholder="Search Exercises"
                     type="text"
                 />
@@ -40,6 +55,7 @@ const SearchExercises = () => {
                         position: "absolute",
                         right: "0",
                     }}
+                    onClick={handleSearch}
                 >
                     Search
                 </Button>
